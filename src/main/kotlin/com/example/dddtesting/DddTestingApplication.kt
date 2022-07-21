50fb200761ff4b4b29e92988d314f1097d48b3ef
xavi <xavierbignell@gmail.com>
2022-07-21 22:54:53 +0200

    first commit

---
+++ src/main/kotlin/com/example/dddtesting/DddTestingApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dddtesting
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DddTestingApplication
+
+fun main(args: Array<String>) {
+	runApplication<DddTestingApplication>(*args)
+}
